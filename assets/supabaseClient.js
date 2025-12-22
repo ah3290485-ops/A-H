6fad085b16004c8469f8d3bb60d0a9a24a135b14
ah3290485-ops <ah3290485@gmail.com>
2025-12-22 21:04:33 +0300

    Update supabaseClient.js

--- assets/supabaseClient.js
+++ assets/supabaseClient.js
@@ -1,8 +1,8 @@
+
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-// 🔴 غيّر القيم التالية
-const SUPABASE_URL = "PUT_YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "PUT_YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://egutbxeysbzvddmpyrkv.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVndXRieGV5c2J6dmRkbXB5cmt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MjE0NDUsImV4cCI6MjA4MTk5NzQ0NX0.DfrpShYXjzSOGSU_IJ4c2sT2WVH4E8Vv2KOLBSDQnwc";
 
 export const supabase = createClient(
   SUPABASE_URL,
